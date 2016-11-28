0b0487e9d8cce0496aa607df5c6cf1c91e7f64f8
vickystamati <vickystam5894@gmail.com>
2016-11-28 17:59:24 +0200

    remove hash.h

--- hash.h
+++
@@ -1,9 +0,0 @@
-struct hashtable{
-	struct list * lista;
-};
-
-void createhash(struct hashtable**,int,int);
-void freegf(int **,int);
-void freehasht(struct hashtable**,int,int);
-void freefix(double ** ,int , int );
-void freehtable(double ***,int ,int );
